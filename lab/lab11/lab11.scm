eb5330d27c6d51af54fbaf3bc8887ca8a0913cfd
Hayeon Jeong <hayeon.jeong@berkeley.edu>
2023-04-11 00:12:20 -0700

    lab11 submit

--- lab/lab11/lab11.scm
+++ lab/lab11/lab11.scm
@@ -1,10 +1,23 @@
-(define (over-or-under num1 num2) 'YOUR-CODE-HERE)
+(define (over-or-under num1 num2) 
+  (cond((< num1 num2) -1)
+       ((= num1 num2) 0)
+       (else 1))
+)
 
-(define (make-adder num) 'YOUR-CODE-HERE)
+(define (make-adder num) 
+  (lambda(inc) (+ num inc))
+)
 
-(define (composed f g) 'YOUR-CODE-HERE)
+(define (composed f g) 
+  (lambda(x) (f (g x)))
+)
 
-(define (repeat f n) 'YOUR-CODE-HERE)
+(define (repeat f n)
+  (lambda(x)
+    (if (= n 0)
+    x
+    ((repeat f (- n 1)) (f x))))
+)
 
 (define (max a b)
   (if (> a b)
