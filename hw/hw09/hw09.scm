525a2269c5333f7bb1c0d18313390d4aa8feff3d
Hayeon Jeong <hayeon.jeong@berkeley.edu>
2023-04-17 09:45:39 -0700

    modify #2

--- hw/hw09/hw09.scm
+++ hw/hw09/hw09.scm
@@ -10,12 +10,14 @@
 
 
 (define (my-filter pred s) 
-    (cond ((null? s) '())
+    (cond ((null? s) s)
 
           ((pred (car s))
-            (cons (car s) (my-filter pred (map pred (cdr s))))
+            (cons (car s) (my-filter pred (cdr s))))
           
-          (else (my-filter pred (map pred (cdr s)))))))
+          (else (my-filter pred (cdr s)))
+    )
+)
 
 
 (define (interleave lst1 lst2)
@@ -24,18 +26,6 @@
           (else (cons (car lst1) (cons (car lst2) (interleave (cdr lst1) (cdr lst2)))))
     )
 )
-
-#|
-(define (no-repeats lst)
-    (cond ((null? lst) lst)
-
-          ((not (= (car lst) (car (cdr lst))))
-            (cons (car lst)
-                (my-filter (lambda (x) (not (= x (car lst))))
-                        (no-repeats (cdr lst)))))
-    )
-)
-|#
 
 (define (no-repeats lst)
   (cond
