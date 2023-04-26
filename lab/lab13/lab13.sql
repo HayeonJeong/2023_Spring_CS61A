b5cc03201e071c8e0e70ffefcf8d79bef852fd2e
Hayeon Jeong <hayeon.jeong@berkeley.edu>
2023-04-25 23:13:02 -0700

    lab13 done

--- lab/lab13/lab13.sql
+++ lab/lab13/lab13.sql
@@ -2,22 +2,44 @@
 
 
 CREATE TABLE bluedog AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT "blue" AS color, "dog" as pet UNION ALL
+  SELECT "blue"         , "dog"        UNION ALL
+  SELECT "blue"         , "dog"        UNION ALL
+  SELECT "blue"         , "dog"        UNION ALL
+  SELECT "blue"         , "dog"        UNION ALL
+  SELECT "blue"         , "dog"        UNION ALL
+  SELECT "blue"         , "dog"        UNION ALL
+  SELECT "blue"         , "dog"        UNION ALL
+  SELECT "blue"         , "dog"        UNION ALL
+  SELECT "blue"         , "dog";
 
 CREATE TABLE bluedog_songs AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT "blue" AS color, "dog" as pet, "Glimpse of Us" as song UNION ALL
+  SELECT "blue"         , "dog"       , "The Other Side of Paradise" UNION ALL
+  SELECT "blue"         , "dog"       , "Leave the Door Open" UNION ALL
+  SELECT "blue"         , "dog"       , "Bohemian Rhapsody" UNION ALL
+  SELECT "blue"         , "dog"       , "Dancing Queen" UNION ALL
+  SELECT "blue"         , "dog"       , "Palette" UNION ALL
+  SELECT "blue"         , "dog"       , "Bohemian Rhapsody" UNION ALL
+  SELECT "blue"         , "dog"       , "good 4 u" UNION ALL
+  SELECT "blue"         , "dog"       , "Clair de Lune" UNION ALL
+  SELECT "blue"         , "dog"       , "Dancing Queen";
 
 
 CREATE TABLE smallest_int_having AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT time, MIN(smallest) FROM students GROUP BY smallest HAVING COUNT(*) == 1;
 
 
 CREATE TABLE matchmaker AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT a.pet, a.song, a.color, b.color
+  FROM students AS a, students AS b
+  WHERE a.pet = b.pet AND a.song = b.song AND a.time < b.time;
 
 
 CREATE TABLE sevens AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT s.seven
+  FROM students AS s, numbers AS n
+  WHERE s.number = 7 AND n."7" = "True" AND s.time = n.time;
 
 
 CREATE TABLE average_prices AS
